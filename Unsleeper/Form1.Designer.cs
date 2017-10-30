b64dcea581b610b5f7724149ba0c71d0ca246c90
John Urbina <j.urbina@pro-s.co.jp>
2017-10-30 14:29:42 +0900

    no message

---
+++ Unsleeper/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Unsleeper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.StartBtn);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(236, 90);
+            this.panel1.TabIndex = 0;
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.StartBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.StartBtn.Location = new System.Drawing.Point(0, 0);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(236, 42);
+            this.StartBtn.TabIndex = 0;
+            this.StartBtn.Text = "Start";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Location = new System.Drawing.Point(0, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 48);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "00:00:00";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(236, 90);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Unsleeper";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button StartBtn;
+    }
+}
+
---
+++ Unsleeper/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Unsleeper
+{
+    public partial class Form1 : Form
+    {
+        public const uint ES_CONTINUOUS = 0x80000000;
+        public const uint ES_SYSTEM_REQUIRED = 0x00000001;
+        public const uint ES_DISPLAY_REQUIRED = 0x00000002;
+        bool started = false;
+        Stopwatch watch = new Stopwatch();
+        Thread timer;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern uint SetThreadExecutionState([In] uint esFlags);
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            if (started)
+            {
+                started = false;
+                StartBtn.Text = "Start";
+                SetThreadExecutionState(ES_CONTINUOUS);
+                watch.Stop();
+                timer.Abort();
+            }
+            else
+            {
+                started = true;
+                StartBtn.Text = "Stop";
+                SetThreadExecutionState(ES_CONTINUOUS | ES_DISPLAY_REQUIRED);
+                watch.Start();
+
+                timer = new Thread(new ThreadStart(new Action(() => {
+                    int sec = 0;
+                    int min = 0;
+                    int hour = 0;
+
+                    while (true)
+                    {
+                        Thread.Sleep(1000);
+                        if (sec == 59) {
+                            sec = 0;
+                            if (min == 59)
+                            {
+                                min = 0;
+                                hour++;
+                            }
+                            else
+                            {
+                                min++;
+                            }
+                        }
+                        else
+                        {
+
+                            sec++;
+                        }
+
+                        this.BeginInvoke((MethodInvoker) delegate () {
+                            label1.Text = hour.ToString("D2") + ":" + min.ToString("D2") + ":" + sec.ToString("D2");
+                        });
+                    }
+                })));
+                timer.Start();
+            }
+        }
+    }
+}
